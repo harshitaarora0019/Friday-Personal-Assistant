23a9a06dc50deb3f705029fae933295a8770f521
Harshita Arora <162481967+harshitaarora0019@users.noreply.github.com>
2025-07-20 15:47:23 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "baby": "https://www.youtube.com/watch?v=kffacxfA7G4",
+    "shape": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc"
+}
